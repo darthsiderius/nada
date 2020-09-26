1c11cd3a1d580420912fc4e51bb4f224d6b7f1b5
Michael Siderius <teddy.addler@gmail.com>
2020-09-26 11:23:32 -0500

    Bulk remove for new prototype

--- nada/__main__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from .nada import main
-main()
--- setup.py
+++
@@ -1,8 +0,0 @@
-
-
-# python 3 dependency
-# operating system
-# create symbolic link
-# add to path
-# run validation
-# welcome page
